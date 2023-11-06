7a75572f863463cec8fc93ef09691a6bf800f612
licfl <474960201@qq.com>
2023-11-06 23:52:25 +0800

    github pages test

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"./", //设置项目的根目录
+  build: {
+    outDir: 'docs' // 打包文件的输出目录
+  },
   plugins: [vue()],
 })
