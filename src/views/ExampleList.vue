756d4d8a17d666f72196532190a821daa018fae9
licfl <474960201@qq.com>
2023-11-08 00:38:38 +0800

    添加示例列表页面

--- src/views/ExampleList.vue
+++ src/views/ExampleList.vue
@@ -1,9 +1,17 @@
 <script setup>
+import example_list from "@/assets/json/example.json"
 
 </script>
 
 <template>
-  这是示例列表页面，展示所有示例
+  <div class="example-list">
+    <div class="example-list-item" v-for="item of example_list">
+      <strong class="example-list-item-title">{{item.title}}</strong>
+      <br />
+      <small class="example-list-item-file">({{item.file}}.html)</small>
+      <p class="example-list-item-discription">{{item.description}}</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
