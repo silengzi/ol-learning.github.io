debc0f13f11debfec0dd7b841307913ce8819f89
李超凡 <lichaofan@newland.com.cn>
2024-02-15 23:50:11 +0800

    报错处理

--- src/views/examples/Device Orientation/index.vue
+++ src/views/examples/Device Orientation/index.vue
@@ -23,7 +23,7 @@
 <script setup>
 import { onMounted } from "vue";
 import { title, html_str, css_str, js_str, package_str } from "./code"
-import "./utils/gyroNorm"
+import "./utils/GyroNorm"
 
 import Map from 'ol/Map.js';
 import OSM from 'ol/source/OSM.js';
